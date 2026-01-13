c9922c6ae285c34e5388fd4030398f22842a9c02
Mona <mona@digityser.com>
2026-01-13 17:04:24 +0100

    [ADD] change svg icone

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -14,8 +14,8 @@
           Dashboard Stats
         </NuxtLink>
         <NuxtLink to="/dashboard" class="nav-item">
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect x="3" y="3" width="7" height="7"></rect><rect x="14" y="3" width="7" height="7"></rect><rect x="14" y="14" width="7" height="7"></rect><rect x="3" y="14" width="7" height="7"></rect></svg>
-          Liste des Articles
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"viewBox="0 0 24 24" fill="none" stroke="currentColor"stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="8" y1="6" x2="21" y2="6" /><line x1="8" y1="12" x2="21" y2="12" /><line x1="8" y1="18" x2="21" y2="18" /><rect x="3" y="5" width="2" height="2" /><rect x="3" y="11" width="2" height="2" /><rect x="3" y="17" width="2" height="2" /></svg>
+           Liste des Articles
         </NuxtLink>
         <NuxtLink to="/favorites" class="nav-item">
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg>
